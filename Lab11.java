3265f0de3472574fa897dab96db8f1661c52de73
HHHjb1 <jonblavigne@gmail.com>
2025-11-14 15:03:42 -0500

    Update Lab11.java

--- Lab11.java
+++ Lab11.java
@@ -1,3 +1,31 @@
+/**
+ * File: Lab11.java
+ * @author Jonathan Forwood, Benji Godo
+ * Created on: Nov 14, 2025
+ * Modified on: Nov 14, 2025
+ * Description:  Lab11
+ */
+
+
+import java.io.DataInputStream;
+import java.io.EOFException;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class Lab11 {
+
+	public static void main(String[] args) throws FileNotFoundException, IOException {
+		try ( // Create an input stream for file pricelist.dat
+				DataInputStream input = new DataInputStream(new FileInputStream("src/pricelist.dat"));
+				) {
+
+			// Read student prices from the file
+			while (true)
+				System.out.println(input.readDouble());
+		} catch (EOFException ex) {	
+			System.out.println("Reached end of file");
+		}
+	}
 
 }
